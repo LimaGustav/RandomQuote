d9ab1abff7bc5d9b2c42a345a8dfcb7e7842328b
unknown <43017102807>
2021-10-29 17:32:16 -0300

    Opa

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default class RandomQuotes extends Component {
       photo: [],
       quote: '',
       urlPhoto: '',
-      randomWord: 'red flower'
+      Word: ''
     }
   }
 
@@ -35,30 +35,15 @@ export default class RandomQuotes extends Component {
     submit.preventDefault();
   }
 
-  randomWord = (submit) => {
-    fetch('https://random-word-api.herokuapp.com/word?number=1')
-
-    .then(response => response.json())
-
-    // .then(resp => this.setState({randomWord: resp[0]}))
-
-    .then(dados => this.setState({randomWord:dados[0]}))
-
-    .then(date => console.log(date[0]))
-  }
 
   searchImage = (submit) => {
     submit.preventDefault();
 
-    const query = this.state.randomWord
+    client.photos.search({ query : this.state.Word, per_page: 1 })
 
-    client.photos.search({ query, per_page: 1 })
+      // .then(response => console.log(response.photos[0].src.original))
 
-    // .then(response => console.log(response.photos[0].src.original))
-
-    .then(response => this.setState({ urlPhoto: response.photos[0].src.original}))
-
-      .then(response => console.log(response.photos[0].src.original))
+      .then(response => console.log(response.photos))
 
       .then(this.cleanQuote)
 
@@ -66,37 +51,47 @@ export default class RandomQuotes extends Component {
   }
 
   cleanQuote = () => {
-    this.setState({quote: ''})
+    this.setState({ quote: '' })
 
     console.log("cheguei aqui")
   }
 
   cleanImage = () => {
-    this.setState({urlPhoto: ''})
+    this.setState({ urlPhoto: '' })
+  }
+
+  randomWord = (submit) => {
+    fetch('https://random-word-api.herokuapp.com/word?number=1')
+
+      .then(response => response.json())
+
+      // .then(dados => console.log(dados[0]))
+
+      .then(word => this.setState({Word: word[0]}))
   }
 
   componentDidMount() {
-
+    this.randomWord();
   }
 
   render() {
     return (
       <main>
-         <img style={this.state.urlPhoto === '' ? {display: 'none'} : {display: 'block'}} className="imagePexel" src={this.state.urlPhoto} alt="Image"></img>
+        <img style={this.state.urlPhoto === '' ? { display: 'none' } : { display: 'block' }} className="imagePexel" src={this.state.urlPhoto} alt="Image"></img>
         <div className="container">
 
-          <h1 style={ this.state.quote === '' ? { display: 'none' } : { display: 'block' } }>
+          <h1 style={this.state.quote === '' ? { display: 'none' } : { display: 'block' }}>
             {
               '"' + this.state.quote + '"'
             }
           </h1>
 
-          <div class="container_form">
+          <div className="container_form">
             <form onSubmit onSubmit={this.listQuotes}>
               <button className="btn" type="submit">Quote</button>
             </form>
             <form onSubmit={this.searchImage} >
-            <button className="btn" type="submit">Image</button>
+              <button className="btn" type="submit">Image</button>
             </form>
           </div>
 
